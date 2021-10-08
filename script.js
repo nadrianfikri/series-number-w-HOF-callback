da25850248a05c214c381afaec5932eacec9ee69
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-10-08 21:47:10 +0700

    add factorial as reduce implements

--- script.js
+++ script.js
@@ -2,35 +2,46 @@ const displayNum = document.querySelector('#display-num p');
 const displayElm = document.getElementById('display-num');
 const numberInput = document.getElementById('number-input');
 
-const evenBtn = document.getElementById('even-btn');
-const oddBtn = document.getElementById('odd-btn');
-const primeBtn = document.getElementById('prime-btn');
+const buttons = document.querySelectorAll('.btn');
 
-evenBtn.addEventListener('click', function () {
-  let result = '';
+function evenNumber() {
+  let results = [];
+
   for (let i = 1; i <= numberInput.value; i++) {
-    if (i % 2 === 0) {
-      result += `${i}  `;
-    }
+    results.push(i);
   }
+  const evenNum = results
+    .filter((result) => {
+      if (result % 2 === 0) {
+        return result;
+      }
+    })
+    .join('  ');
+
   displayElm.style.border = '3px solid #9c1515 ';
-  displayNum.innerHTML = result;
-});
+  displayNum.innerHTML = evenNum;
+}
 
-oddBtn.addEventListener('click', function () {
-  let result = '';
+function oddNumber() {
+  let results = [];
+
   for (let i = 1; i <= numberInput.value; i++) {
-    if (i % 2 === 1) {
-      result += `${i}  `;
-    }
+    results.push(i);
   }
+  const oddNum = results
+    .filter((result) => {
+      if (result % 2 === 1) {
+        return result;
+      }
+    })
+    .join('  ');
+
   displayElm.style.border = '3px solid #f3ef18 ';
+  displayNum.innerHTML = oddNum;
+}
 
-  displayNum.innerHTML = result;
-});
-
-primeBtn.addEventListener('click', function () {
-  let result = '';
+function primeNumber() {
+  let results = [];
 
   function isPrime(num) {
     for (let i = 2; i < num; i++) {
@@ -42,56 +53,59 @@ primeBtn.addEventListener('click', function () {
   }
 
   for (let i = 2; i <= numberInput.value; i++) {
-    if (isPrime(i)) {
-      result += `${i}  `;
-    }
+    results.push(i);
   }
+  const primeNum = results
+    .filter((result) => {
+      if (isPrime(result)) {
+        return result;
+      }
+    })
+    .join('  ');
+
   displayElm.style.border = '3px solid #27bd27 ';
+  displayNum.innerHTML = primeNum;
+}
+function factorial() {
+  if (numberInput.value > 20) {
+    alert('maximum number value is 20');
+    return;
+  }
 
-  displayNum.innerHTML = result;
-});
+  let results = [];
+  for (let i = 2; i <= numberInput.value; i++) {
+    results.push(i);
+  }
 
-// function performEvenNumber() {
-//   let result = '';
-//   for (let i = 1; i <= numberInput.value; i++) {
-//     if (i % 2 === 0) {
-//       result += `${i}  `;
-//     }
-//   }
-//   displayElm.style.border = '3px solid #9c1515 ';
+  let result = results.reduce((acc, cur) => acc * cur);
 
-//   displayNum.innerHTML = result;
-// }
-// function performOddNumber() {
-//   let result = '';
-//   for (let i = 1; i <= numberInput.value; i++) {
-//     if (i % 2 === 1) {
-//       result += `${i}  `;
-//     }
-//   }
-//   displayElm.style.border = '3px solid #f3ef18 ';
+  let reverse = result.toString().split('').reverse().join('');
+  let regex = reverse.match(/\d{1,3}/g);
 
-//   displayNum.innerHTML = result;
-// }
+  const factorial = regex.join('.').split('').reverse().join('');
 
-// function performPrimeNumber() {
-//   let result = '';
+  displayElm.style.border = '3px solid #9c1515 ';
+  displayNum.innerHTML = factorial;
+}
 
-//   function isPrime(num) {
-//     for (let i = 2; i < num; i++) {
-//       if (num % i === 0) {
-//         return false;
-//       }
-//     }
-//     return true;
-//   }
+for (let button of buttons) {
+  button.addEventListener('click', function (e) {
+    const target = e.target;
 
-//   for (let i = 2; i <= numberInput.value; i++) {
-//     if (isPrime(i)) {
-//       result += `${i}  `;
-//     }
-//   }
-//   displayElm.style.border = '3px solid #27bd27 ';
+    if (target.classList.contains('even')) {
+      evenNumber();
+    }
 
-//   displayNum.innerHTML = result;
-// }
+    if (target.classList.contains('odd')) {
+      oddNumber();
+    }
+
+    if (target.classList.contains('prime')) {
+      primeNumber();
+    }
+
+    if (target.classList.contains('factorial')) {
+      factorial();
+    }
+  });
+}
